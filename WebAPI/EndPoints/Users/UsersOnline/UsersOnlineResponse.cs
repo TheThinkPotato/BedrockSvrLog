39cdb6ef5e4d2dc0912fa7df67346e96c5bcfb2f
Dan Lopez <86757951+TheThinkPotato@users.noreply.github.com>
2025-08-09 18:55:48 +1000

    Added userOnline Endpoint

---
+++ WebAPI/EndPoints/Users/UsersOnline/UsersOnlineResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace WebAPI;
+public class UsersOnlineResponse
+{
+    public List<UserOnlineDTO> Users { get; set; } = [];
+}
+
+public record UserOnlineDTO
+{
+    public string Name { get; init; } = string.Empty;
+    public string Xuid { get; init; } = string.Empty;
+    public string? Pfid { get; init; } = null;
+    public DateTime LastLogin { get; init; }
+    public DateTime SpawnTime { get; init; }
+    public TimeSpan TimeSinceLastLogin => DateTime.Now - LastLogin;
+    public TimeSpan TimeSinceSpawn => DateTime.Now - SpawnTime;
+}
