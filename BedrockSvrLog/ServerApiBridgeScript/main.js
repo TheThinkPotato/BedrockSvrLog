d9281abb6c5852fbe66f7664b727c61a58d3f306
Dan Lopez <86757951+TheThinkPotato@users.noreply.github.com>
2025-08-23 10:45:05 +1000

    update api bridge script
    Update main.js

--- BedrockSvrLog/ServerApiBridgeScript/main.js
+++ BedrockSvrLog/ServerApiBridgeScript/main.js
@@ -1,23 +1,54 @@
-﻿import { system, world, EntityTypes } from "@minecraft/server";
+﻿import { world } from "@minecraft/server";
 
 world.afterEvents.entityDie.subscribe((eventData) => {
-const entity = eventData.deadEntity;
-const killer = eventData.damageSource?.damagingEntity;
+  const deadEntity = eventData.deadEntity;
 
-// If it's a player that died
-if (entity.typeId === "minecraft:player") {
-    console.log(`Scipt Player: ${entity.nameTag} has died! at ${eventData.location} by ${eventData.damageSource?.typeId}`);
-    } else {
-        // If it’s an animal (or any mob)
-        console.log(`Scipt Enity Died: ${entity.typeId}`);
+  const getLocation = () => {
+    if (deadEntity) {
+      const location = {
+        x: deadEntity.location.x,
+        y: deadEntity.location.y,
+        z: deadEntity.location.z,
+      };
+      return location;
     }
+    return "Unknown Location";
+  };
 
-// Bonus: if there was a killer
-if (killer) {
-        if (killer.typeId === "minecraft:player") {
-        console.log(`Script Killed by player: ${killer.nameTag}`);
-        } else {
-        console.log(`Script Killed by: ${killer.typeId}`);
-        }
+  const getKiller = () => {
+    const killer = eventData.damageSource?.damagingEntity;
+    const cause = eventData.damageSource?.cause;
+    const causeProjectile = eventData.damageSource?.projectile;
+
+    if (killer) {
+      if (killer.typeId === "minecraft:player") {
+        return `player - ${killer.nameTag}`;
+      } else if (killer.typeId) {
+        return killer.typeId;
+      }
     }
+    if (cause) {
+      return cause;
+    }
+    if (causeProjectile) {
+      return causeProjectile.typeId;
+    }
+    return "Unknown";
+  };
+
+  // If it's a player that died
+  if (deadEntity.typeId === "minecraft:player") {
+    console.log(
+      `Script Player Death: ${
+        deadEntity.nameTag
+      } at: ${getLocation()} by: ${getKiller()}`
+    );
+  } else {
+    // If it’s an animal (or any mob)
+    console.log(
+      `Script Entity Death: ${
+        deadEntity.typeId
+      } at: ${getLocation()} by: ${getKiller()}`
+    );
+  }
 });
