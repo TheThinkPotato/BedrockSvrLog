01af0a5236392581c4726c578ea3d87e7ee39617
Dan Lopez <86757951+TheThinkPotato@users.noreply.github.com>
2025-08-28 23:14:13 +1000

    Updated websocket hub into worldContext

--- WebApp/bedrock-svr-log/src/main.tsx
+++ WebApp/bedrock-svr-log/src/main.tsx
@@ -2,11 +2,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WorldProvider } from './context/WorldContext.tsx'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
+    <WorldProvider>
     <App />
+    </WorldProvider>
   </QueryClientProvider>
 )
