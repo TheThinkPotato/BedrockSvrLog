89ae31a3edaa73474489bc25f3b8bfb09fd1bb66
Dan Lopez <86757951+TheThinkPotato@users.noreply.github.com>
2025-09-04 12:31:43 +1000

    Add User Player Kill endpoint to WebAPI

---
+++ WebAPI/EndPoints/User/PlayerKIlls/UserPlayerKillsEndpoint.cs
@@ -0,0 +1,52 @@
+﻿using FastEndpoints;
+using BedrockSvrLog.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.EndPoints.User.PlayerKIlls;
+
+public class UserPlayerKillsEndpoint : EndpointWithoutRequest<PlayerKillsResponse>
+{
+    private readonly AppDbContext _db;
+
+    public UserPlayerKillsEndpoint(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public override void Configure()
+    {
+        Get("/api/User/{xuid}/PlayerKills");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var xuid = Route<string>("xuid");
+
+        var userPlayerKills = await _db.User
+                                .Where(u => u.Xuid == xuid)
+                                .Join(_db.PlayerKills, u => u.Xuid, pk => pk.Xuid, (u, pk) => new { u, pk })
+                                .GroupBy(upk => new { upk.u.Xuid, upk.u.Name })
+                                .ToListAsync(ct);
+
+        var playerKills = userPlayerKills.Select(g => new PlayerKills
+        {
+            Xuid = g.Key.Xuid,
+            Name = g.Key.Name,
+            TotalKills = g.Count(),
+            PlayerKillsList = g.GroupBy(x => x.pk.EntityType)
+                                        .Select(eg => new PlayerKillList
+                                        {
+                                            EntityType = eg.Key,
+                                            KillCount = eg.Count()
+                                        }).ToList()
+        }).ToList();
+
+        var response = new PlayerKillsResponse
+        {
+            PlayerKills = playerKills
+        };
+
+        await Send.OkAsync(response, ct);
+    }
+}
---
+++ WebAPI/EndPoints/User/PlayerKIlls/UserPlayerKillsResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace WebAPI.EndPoints.User.PlayerKIlls;
+
+public class UserPlayerKillsResponse
+{
+    public string Xuid { get; init; } = string.Empty;
+    public string Name { get; init; } = string.Empty;
+    public int TotalKills { get; init; } = 0;
+    public string FavouriteKillEntity => PlayerKillsList.OrderByDescending(pk => pk.KillCount).FirstOrDefault()?.EntityType ?? "None";
+
+    public List<PlayerKillListUser> PlayerKillsList { get; init; } = [];
+
+}
+
+public record PlayerKillListUser
+{
+    public string EntityType { get; init; } = string.Empty;
+    public int KillCount { get; init; } = 0;
+}
